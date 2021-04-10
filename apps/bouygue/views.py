f7c5a52a44a1662230c7504891a8fac23f789ca7
GuillaumeM92 <gemacx@gmail.com>
2021-04-10 22:55:10 +0200

    sentry debug

--- apps/bouygue/views.py
+++ apps/bouygue/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from apps.users.models import MyUser
 from apps.blog.models import Post
 from apps.info.models import InfoPost
-
 
 def landing(request):
     if request.user.is_authenticated:
@@ -16,6 +14,3 @@ def home(request):
     posts_length = len(Post.objects.all()) - user.discussions_read
     infoposts_length = len(InfoPost.objects.all()) - user.informations_read
     return render(request, 'bouygue/home.html', {'title': 'Home', 'posts_length': posts_length, 'infoposts_length': infoposts_length})
-
-def trigger_error(request):
-    division_by_zero = 1 / 0
