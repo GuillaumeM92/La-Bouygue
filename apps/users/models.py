117b4934523156dfd6cfc78fa591c4a6ef8292ab
GuillaumeM92 <gemacx@gmail.com>
2021-04-10 23:55:08 +0200

    img upload resizing fix

--- apps/users/models.py
+++ apps/users/models.py
@@ -73,7 +73,7 @@ class Profile(models.Model):
         img = Image.open(self.image.path)
 
         if img.height > 300 or img.width > 300:
-            output_size = (img.height/(img.height // 300), img.width/(img.width // 300))
+            output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
 
