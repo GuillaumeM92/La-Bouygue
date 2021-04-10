e3ad57e182d22a7f3c56278f9ea13238532d5bdf
GuillaumeM92 <gemacx@gmail.com>
2021-04-10 19:23:59 +0200

    configured static files

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,4 +11,4 @@ urlpatterns = [
     path('', include('apps.info.urls')),
     path('', include('apps.work.urls')),
     path('', include('apps.budget.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
