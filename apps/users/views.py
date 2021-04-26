bd4855dc8c22fbec8fb71dad6665efd2bee57916
GuillaumeM92 <gemacx@gmail.com>
2021-04-26 15:00:31 +0200

    added support email to data policy page

--- apps/users/views.py
+++ apps/users/views.py
@@ -29,8 +29,8 @@ def register(request):
             send_mail("La Bouygue - Nouvel Utilisateur",
                       ("{} {} s'est créé un compte sur le site de la Bouygue. Merci de vérifier "
                        "son identité avant d'activer son compte. Lien : "
-                       "https://labouygue.com/admin/activate/").format(user_surname, user_name),
-                      None, ["gemacx@gmail.com", "aurelieporcq@gmail.com"], fail_silently=True, )
+                       "https://labouygue.com/info/admin/activate/").format(user_surname, user_name),
+                      None, ["gemacx@gmail.com"], fail_silently=True, )
             messages.success(request, str(
                 "Votre compte a été créé avec succès ! Un email de confirmation vient de vous "
                 "être envoyé. Vous pourrez vous connecter dès qu'un administrateur aura "
