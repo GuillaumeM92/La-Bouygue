6f5e50da34eee64e969bf10d7137e3bc0752b621
GuillaumeM92 <gemacx@gmail.com>
2021-05-11 14:42:38 +0200

    fixed all users length

--- apps/info/views.py
+++ apps/info/views.py
@@ -212,3 +212,8 @@ class AllUsersListView(LoginRequiredMixin, ListView):
     context_object_name = 'users'
     ordering = ['surname']
     paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["all_users"] = MyUser.objects.all()
+        return context
