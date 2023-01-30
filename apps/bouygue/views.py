08061977d3e078d72dea0f17c5a0640e37b0c06b
GuillaumeM92 <gemacx@gmail.com>
2023-01-30 22:37:12 +0100

    Make the caroussel display 5 random images.

--- apps/bouygue/views.py
+++ apps/bouygue/views.py
@@ -6,8 +6,7 @@ from apps.blog.models import Post, Comment
 from apps.activities.models import Activity
 from apps.info.models import InfoPost
 from apps.work.models import Work
-# import random
-from random import shuffle
+import random
 
 
 def landing(request):
@@ -17,24 +16,10 @@ def landing(request):
         return render(request, 'bouygue/landing.html')
 
 
-# def get_random_image(queryset):
-#     rand_int = random.randint(0, queryset.count() - 1)
-#     return queryset[rand_int].image
+def get_random_image(queryset):
+    rand_int = random.randint(0, queryset.count() - 1)
+    return queryset[rand_int].image
 
-
-def get_all_image_urls():
-    img_urls_list = []
-    # get posts and comments that contain images
-    posts_with_images = Post.objects.exclude(image='')
-    comments_with_images = Comment.objects.exclude(image='')
-    # fill the list with image urls
-    for post in posts_with_images:
-        img_urls_list.append(post.image.url)
-    for comment in comments_with_images:
-        img_urls_list.append(comment.image.url)
-    shuffled_list = shuffle(img_urls_list)
-    return shuffled_list
-    
 
 @login_required
 def home(request):
@@ -45,13 +30,15 @@ def home(request):
     infoposts_length = len(InfoPost.objects.all()) - user.informations_viewed
     works_length = len(Work.objects.all()) - user.works_viewed
     # get posts and comments that contain images
-    # posts_with_images = Post.objects.exclude(image='')
-    # comments_with_images = Comment.objects.exclude(image='')
-    # get 3 random images for the caroussel (and make sure not to pick the same one twice)
-    # caroussel_img_1 = get_random_image(posts_with_images)
-    # caroussel_img_2 = get_random_image(posts_with_images.exclude(image=caroussel_img_1))
-    # caroussel_img_3 = get_random_image(comments_with_images)
-
+    posts_with_images = Post.objects.exclude(image='')
+    comments_with_images = Comment.objects.exclude(image='')
+    # get 5 random images for the caroussel (and make sure not to pick the same one twice)
+    caroussel_img_1 = get_random_image(posts_with_images)
+    caroussel_img_2 = get_random_image(comments_with_images)
+    caroussel_img_3 = get_random_image(posts_with_images.exclude(image=caroussel_img_1))
+    caroussel_img_4 = get_random_image(comments_with_images.exclude(image=caroussel_img_2))
+    caroussel_img_5 = get_random_image(comments_with_images.exclude(image=caroussel_img_2).exclude(image=caroussel_img_4))
+    
     response = render(request, 'bouygue/home.html', {
         'title': 'Accueil',
         'reservations_length': reservations_length,
@@ -59,10 +46,11 @@ def home(request):
         'activities_length': activities_length,
         'infoposts_length': infoposts_length,
         'works_length': works_length,
-        'all_image_urls_list': get_all_image_urls()
-        # 'caroussel_img_1': caroussel_img_1,
-        # 'caroussel_img_2': caroussel_img_2,
-        # 'caroussel_img_3': caroussel_img_3
+        'caroussel_img_1': caroussel_img_1,
+        'caroussel_img_2': caroussel_img_2,
+        'caroussel_img_3': caroussel_img_3,
+        'caroussel_img_4': caroussel_img_4,
+        'caroussel_img_5': caroussel_img_5
     })
     return response
 
