6a03fce89f83fbf4a2c331c0ebaf62ebad2bc4d8
GuillaumeM92 <gemacx@gmail.com>
2024-03-07 21:10:38 +0100

    Update management command.

--- apps/users/management/commands/send_mail_to_all.py
+++ apps/users/management/commands/send_mail_to_all.py
@@ -10,13 +10,13 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         users = MyUser.objects.all()
         user_emails = [user.email for user in users]
-        user_emails = ["gemacx@gmail.com"]
         self.stdout.write(self.style.SUCCESS(f'Sending email to {user_emails}'))
         send_mail(
-            subject="La Bouygue - Compte Activé",
+            subject="La Bouygue - Changement d'adresse pour le site web",
             message=(
-                "Le site change d'adresse !"
-                "Pour réduire les coûts, le site va être déplacé de labouygue.com vers labouygue.fr"
+                "Le site change d'adresse !\n\n"
+                "Pour réduire les coûts, le site a été déplacé de labouygue.com vers labouygue.fr\n"
+                "Le domaine en '.com' cessera d'être accessible à compter du 12 Avril prochain."
             ),
             from_email=None,
             recipient_list=user_emails,
